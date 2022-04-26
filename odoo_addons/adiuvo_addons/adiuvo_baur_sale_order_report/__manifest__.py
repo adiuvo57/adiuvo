aaa0d1384255a078856223e24eb9f30b98047db4
Martin Tanner <73464125+adiuvo57@users.noreply.github.com>
2022-04-26 16:46:38 +0200

    Create __manifest__.py

---
+++ odoo_addons/adiuvo_addons/adiuvo_baur_sale_order_report/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'Customized Sale Report',
+    'version' : '0.1',
+    'author' : 'adiuvo Consulting GmbH',
+    'category' : 'Sales',
+    'website': 'https://www.adiuvo.ch',
+    'license': 'AGPL-3',
+    'description' : """
+**Features list :**
+    * Details zu Customized Sale Report.
+""",
+    'depends' : [
+        'sales'
+    ],
+    'data': [
+
+    ],
+    'installable': True,
+    'auto_install': False,
+    'images': [
+    ]
+}
